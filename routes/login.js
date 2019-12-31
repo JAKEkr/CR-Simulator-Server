2e283544075ad404e6119bab169c8031a1566a2b
Jake Lee <secujake@gmail.com>
2019-12-31 14:14:58 +0900

    Add feature to login

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,23 @@
 "use strict";
 
 const router = require('express').Router();
+const User = require('../models/user');
 
+router.post('/', function(req, res) {
+    User.findOne({student_id: req.body.student_id}, function(err, user) {
+        return new Promise(function (resolve, reject) {
+            if(!user || err)
+                reject({success: 'false'});
 
+            user.comparePassword(req.body.password, function (err, isMatch) {
+                if(!isMatch || err)
+                    reject({success: 'false'});
+                else
+                    resolve({success: 'true'});
+            });
+        }).then(msg => res.send(msg))
+            .catch(err => res.send(err));
+    });
+});
 
 module.exports = router;
